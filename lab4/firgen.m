cb057aeb08ac7ef51ce63f05a6b7004c1a30e95a
Thomas Lim <txl11@ic.ac.uk>
2014-03-06 11:27:51 +0000

    Updated firgen function to export the coefficients to a text file (and that is not gitignore-d)

--- lab4/firgen.m
+++ lab4/firgen.m
@@ -4,10 +4,13 @@ fs = 8000;        % Sampling frequency
 f = [240 440 2000 2300];    % Cutoff frequencies
 a = [0 1 0];        % Desired amplitudes
 % Compute deviations
-dev = [10^(-rs/20) (10^(rp/20)-1)/(10^(rp/20)+1)  10^(-rs/20)]
-[n,fo,ao,w] = firpmord(f,a,dev,fs)
-b = firpm(n,fo,ao,w);
+dev = [10^(-rs/20) (10^(rp/20)-1)/(10^(rp/20)+1)  10^(-rs/20)];
+
+[n,fo,ao,w] = firpmord(f,a,dev,fs);
+
+b = firpm(n,fo,ao,w)
+
+save('RTDSP/coeffs.txt', 'b','-ascii', '-double', '-tabs')
+
 freqz(b,1,1024,fs);
 title('Lowpass Filter Designed to Specifications');
-
-% f = [.03 .055 .25 .30625]; % these correspond to the different frequencies in proportion to the nyquist plot
