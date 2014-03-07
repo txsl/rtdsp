045993ffee03594d6d67ce54dfee3a1e01c124e1
Thomas <txl11@imperial.ac.uk>
2014-03-07 16:17:04 +0000

    firgen.m is now actually printing directly to the coefficient file which is being included

--- lab4/firgen.m
+++ lab4/firgen.m
@@ -8,9 +8,25 @@ dev = [10^(-rs/20) (10^(rp/20)-1)/(10^(rp/20)+1)  10^(-rs/20)];
 
 [n,fo,ao,w] = firpmord(f,a,dev,fs);
 
-b = firpm(n+10,fo,ao,w)
+b = firpm(n+10,fo,ao,w);
 
 save('RTDSP/coeffs.txt', 'b','-ascii', '-double', '-tabs')
 
-freqz(b,1,1024,fs);
-title('Lowpass Filter Designed to Specifications');
+out = 'double b[] = {';
+
+for i=1:length(b)
+    if i==1
+        out = [out, sprintf('%1.16e', b(i))];
+    else
+        out = [out, ', ', sprintf('%1.16e', b(i))];
+    end
+    
+end
+out = [out, '};']
+
+f = fopen('RTDSP/coeffs.txt', 'w+');
+fprintf(f, out);
+fclose(f);
+
+% freqz(b,1,1024,fs);
+% title('Lowpass Filter Designed to Specifications');
