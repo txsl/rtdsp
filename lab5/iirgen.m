2e833c5802231df6cdc6bafaf96a741842438c84
Thomas <txl11@imperial.ac.uk>
2014-03-14 16:34:19 +0000

    Cleaned up iirgen file so it's only producing what we need

--- lab5/iirgen.m
+++ lab5/iirgen.m
@@ -4,11 +4,9 @@ rs = 25;          % Stopband ripple
 fs = 8000;        % Sampling frequency
 pb = [280 470];
 
-[z,p,k] = ellip(ord, rp, rs, pb/(fs/2));
-[a,b] = ellip(ord, rp, rs, pb/(fs/2))
-[sos,g] = zp2sos(z,p,k);
-Hd = dfilt.df2tsos(sos,g);
-h = fvtool(Hd);
+[a,b] = ellip(ord/2, rp, rs, pb/(fs/2));
+
+freqz(a, b,10)
 
 out = 'double a[] = {';
 
