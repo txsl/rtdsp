2e622dde432b73ac60dc91234aa29614b62f557b
Thomas Lim <txl11@ic.ac.uk>
2015-12-26 21:42:15 +0000

    Commit some rando code I still had kicking about on my Mac

--- lab4/format.py
+++ lab4/format.py
@@ -1,18 +1,20 @@
-handle = open('RTDSP/coeffs.txt', 'r+')
+handle = open('RTDSP/coeffs.txt', 'r+') # open the file and read its contents
 coeffs = handle.readline()
 handle.close()
 
-split = coeffs.split('\t')
+split = coeffs.split('\t') # split the file by the tab delimiter which matlab exported it
 
-out = 'double b[] = {'
+out = 'double b[] = {' # the start of the string 
 
-for c in split:
-	if c == '\n':
+# we iterate through the coefficients and append them to the string, with a comma
+for c in split: 
+	if c == '\n':  # if there is a newline character, we ignore that iteration
 		continue
 	out = out + c + ','
 
-out = out[0:-1]
-out = out + '}'
+out = out[0:-1] # take the last comma from the string 
+out = out + '}' # add the closing brace
 
+# overwrite the original file with this output
 new = open('RTDSP/coeffs.txt', 'w+')
 new.write(out)
